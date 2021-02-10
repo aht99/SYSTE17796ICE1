bb1caf6a00336ad771e88ea9f839856abff5a939
aht99 <78034679+aht99@users.noreply.github.com>
2021-02-09 21:08:18 -0500

    Update Main.java

--- src/calcavg/Main.java
+++ src/calcavg/Main.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         Scanner input = new Scanner(System.in);
         
-        int[] grade = new int[10];
+        int[] marks = new int[10];
         int total = 0;
         int average = 0;
         System.out.println("Enter 10 grades for average: ");
         
-        for(int i = 0; i < grade.length; i++){
-            grade[i] = input.nextInt();
-            if(grade[i] < 0){
+        for(int i = 0; i < marks.length; i++){
+            marks[i] = input.nextInt();
+            if(marks[i] < 0){
                 break;
             }
-            total += grade[i];
+            total += marks[i];
         }
         average = total / 10;
         
