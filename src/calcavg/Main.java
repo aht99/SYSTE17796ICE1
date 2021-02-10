66a668a6fdff13f683af762d924ce9289bcb2c4f
Alexander Tran <tran62@sheridancollege.ca>
2021-02-09 21:31:54 -0500

    First commit

--- src/calcavg/Main.java
+++ src/calcavg/Main.java
@@ -20,9 +20,21 @@ public class Main {
             }
             total += marks[i];
         }
+        calcMax(marks);
+        
         average = total / 10;
         
         System.out.println("Average: " + average);
     }
     
+    public static int calcMax(int[] numbers){
+        int maxValue = numbers[0];
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] > maxValue){
+                maxValue = numbers[i];
+            }
+        }
+        System.out.println("Maximum number = " + maxValue);
+        return maxValue;
+    }
 }
